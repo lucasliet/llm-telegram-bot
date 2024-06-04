3620c802714b13557da6b14aad1a1c62f2bd09bb
lucasliet <lucasouliveira@gmail.com>
2024-06-04 00:05:58 -0300

    cloud stable test

--- main.ts
+++ main.ts
@@ -1,11 +1,13 @@
 import { Application } from 'https://deno.land/x/oak@v12.6.1/mod.ts';
 import { oakCors } from 'https://deno.land/x/cors@v1.2.2/mod.ts';
 
-import { Bot, webhookCallback } from 'https://deno.land/x/grammy@v1.17.2/mod.ts';
+import { Bot, InputFile, webhookCallback } from 'https://deno.land/x/grammy@v1.17.2/mod.ts';
 import { replyTextContent } from './service/TelegramService.ts';
 import { clearChatHistory } from './repository/ChatRepository.ts';
 
 const TOKEN: string = Deno.env.get('BOT_TOKEN') as string;
+const CLOUDFLARE_ACCOUNT_ID: string = Deno.env.get('CLOUDFLARE_ACCOUNT_ID') as string;
+const CLOUDFLARE_API_KEY: string = Deno.env.get('CLOUDFLARE_API_KEY') as string;
 const PORT: number = parseInt(Deno.env.get('PORT') as string) || 80;
 
 const BOT = new Bot(TOKEN);
@@ -28,6 +30,18 @@ BOT.command('clear', async (ctx) => {
   await ctx.reply('Histórico de conversa apagado com sucesso!');
 });
 
+BOT.command('image', async (ctx) =>{
+  const response = await fetch(`https://api.cloudflare.com/client/v4/accounts/${CLOUDFLARE_ACCOUNT_ID}/ai/run/@cf/lykon/dreamshaper-8-lcm`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      'Authorization': `Bearer ${CLOUDFLARE_API_KEY}`
+    },
+    body: JSON.stringify({ prompt: "cyberpunk cat in anime art style battling in the moon" })
+  });
+  ctx.replyWithPhoto(new InputFile(new Uint8Array(await response.arrayBuffer()), 'image/png'));
+});
+
 BOT.on('message', async (ctx) => {
   console.info(`user: ${ctx.msg.from?.id}, message: ${ctx.message?.text}`);
   try {
@@ -41,7 +55,7 @@ BOT.on('message', async (ctx) => {
 
 APP.use(async (ctx, next) => {
   try {
-    if (ctx.request.url.pathname !== '/webhook') {
+    if (ctx.request.url.pathname !== '/webhook' && !ctx.request.url.pathname.startsWith('/image')) {
       ctx.response.status = 200;
       ctx.response.body = 'Use with https://t.me/llm_gemini_bot';
       return;
